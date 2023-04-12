dcfeb51320078889d84a9f81372d7c75708145e7
tienbot1 <tien.ru@mail.ru>
2023-04-12 23:46:13 +0900

    city search is work

--- script.js
+++ script.js
@@ -9,39 +9,60 @@ const success = function (data) {
 
 const error = function () {
     console.error("ERROR")
-    chooseCity.classList.add('d-none')
+    chooseCityDiv.classList.add('d-none')
     weatherDiv.classList.add('d-none')
+    errorDiv.classList.remove('d-none')
 }
 
 navigator.geolocation.getCurrentPosition(success, error);
 
-// let lat = 50.2727763
-// let lon = 127.5404017
 let lat = null
 let lon = null
 let api = '97109d73f740c94c7c42ac8c357cc62f'
 const inputCity = document.getElementById('inputCity')
+const tryAgain = document.getElementById('tryAgain')
 let cityName = null
-let weatherDiv = document.querySelector('.weather')
-let chooseCity = document.querySelector('.chooseCity')
+let weatherDiv = document.querySelector('.weatherDiv')
+let chooseCityDiv = document.querySelector('.chooseCityDiv')
+let errorDiv = document.querySelector('.errorDiv')
 
 inputCity.addEventListener('change', ()=>{
     cityName = inputCity.value
-    console.log(cityName)
     inputCity.value = ''
-    city()
-    chooseCity.classList.add('d-none')
-    weatherDiv.classList.remove('d-none')
+    tempResult = city()
+    tempResult.then(data => dataAtSite(data))
+    // if(datа.cod === '404'){
+    //     errorDiv.classList.remove('d-none')
+    //     weatherDiv.classList.add('d-none')
+    //     chooseCityDiv.classList.add('d-none')
+    // } else {
+        chooseCityDiv.classList.add('d-none')
+        weatherDiv.classList.remove('d-none')
+    // }
+})
+
+tryAgain.addEventListener('click', ()=>{
+    chooseCityDiv.classList.remove('d-none')
+    errorDiv.classList.add('d-none')
+    weatherDiv.classList.addc('d-none')
+    // lat = 50.2727763
+    // lon = 127.5404017
+    tempResult = temp(lat, lon);
+    tempResult.then(data => dataAtSite(data))
 })
 
 async function city(){
-    const geo = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}`)
+    const geo = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=ru&appid=${api}`)
     const data = await geo.json()
-    console.log(data)
+    console.log(data.cod)
+    //если введен несуществующий город
+    if(data.cod === '404'){
+        errorDiv.classList.remove('d-none')
+        weatherDiv.classList.add('d-none')
+        chooseCityDiv.classList.add('d-none')
+    }
     return data
 }
-const cityResult = await city()
-console.log(cityResult)
 
 async function temp(lat, lon){
     const weather = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=ru&appid=${api}`)
@@ -50,7 +71,8 @@ async function temp(lat, lon){
 }
 
 function dataAtSite(obj) {
-    const weatherDiv = document.body.querySelector(".weather")
+    const weatherDiv = document.body.querySelector(".weatherDiv")
+    weatherDiv.innerHTML=''
     //создаем карточку
     const p = document.createElement('p')
     const h1 = document.createElement('h1')
@@ -65,14 +87,28 @@ function dataAtSite(obj) {
     weather = weather.join('')
     const button = document.createElement('button')
     button.innerHTML='Change city'
+    button.setAttribute("class", 'changeCity')
     button.addEventListener('click', ()=>{
-        chooseCity.classList.remove('d-none')
+        chooseCityDiv.classList.remove('d-none')
         weatherDiv.classList.add('d-none')
-        dataAtSite(cityResult)
     })
     
+
     h1.innerHTML=`${cel}℃`
-    p.innerHTML=`${weather} в ${city}е`
+    //склонение города в зависимости от последней буквы
+    if (city[city.length-1] == 'а'){
+        city=city.split('')
+        city.pop('а')
+        city.push('е')
+        city = city.join('')
+    } else if(city[city.length-1] == 'и'){
+        city=city
+    } else {
+        city=city.split('')
+        city.push('е')
+        city = city.join('')
+    }
+    p.innerHTML=`${weather} в ${city}`
 
     weatherDiv.append(img, h1, p, button)
 }
